daeaa3b4ecd092a5f04ac6a61accc062d5d139a2
tiagosouza <tiagosouza@fametro.edu.br>
2023-03-09 18:18:05 -0400

    prod

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -9,10 +9,10 @@ import { Observable, retry } from "rxjs";
 
 export class AppService{
   
-  urlCadastrar = 'http://localhost:8080/user/signup';
-  urlLogin = 'http://localhost:8080/user/login';
-  urlEsqueceuSenha = 'http://localhost:8080/user/forgotpassword';
-  urlcheckToken ='http://localhost:8080/user/checkToken'
+  urlCadastrar = 'https://back-sta.herokuapp.com/user/signup';
+  urlLogin = 'https://back-sta.herokuapp.com/user/login';
+  urlEsqueceuSenha = 'https://back-sta.herokuapp.com/user/forgotpassword';
+  urlcheckToken ='https://back-sta.herokuapp.com/user/checkToken'
 
   constructor(private _http:HttpClient) { }
    
